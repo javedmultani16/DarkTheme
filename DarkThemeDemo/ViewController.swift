7ddf60d65d8cfb57907d98a047d1f55897b1a6f1
Javid Multani <javid.multani@contentcreation.com.my>
2019-09-21 15:22:52 +0800

    initial commit

---
+++ DarkThemeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkThemeDemo
+//
+//  Created by CCC on 21/09/2019.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
